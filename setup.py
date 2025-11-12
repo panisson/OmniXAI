eead25c1bed846a7582b050738bf8783a7cf2fe2
André Panisson <panisson@gmail.com>
2025-11-12 15:45:40 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setup(
     install_requires=[
         "numpy>=1.17,<=1.26",
         "pandas>=1.1.0",
-        "scikit-learn>=0.24,<1.2",
+        "scikit-learn>=0.24",
         "scipy>=1.5.0",
         "scikit-image>=0.17.2",
         "matplotlib",
